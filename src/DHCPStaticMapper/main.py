2bdb351e840c9bb959498c8a674605e1e233590e
JKearnsl <boyarshinovn@gmail.com>
2024-01-17 00:19:53 +0300

    Обновлена логика работы приложения

--- src/DHCPStaticMapper/main.py
+++ src/DHCPStaticMapper/main.py
@@ -7,7 +7,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from DHCPStaticMapper.auth import check_access, auth
 from DHCPStaticMapper.config import load_env_config, Config
 from DHCPStaticMapper.dhcp_leases import dhcp_dynamic_leases_table
-from DHCPStaticMapper.utils import parse_csrf
+from DHCPStaticMapper.utils import parse_csrf, parse_ntp, parse_dns
 
 
 def make_static_dhcp(http_client: httpx.Client, config: Config):
@@ -23,7 +23,35 @@ def make_static_dhcp(http_client: httpx.Client, config: Config):
             return
 
     # Resources
-    dhcp_leases = dhcp_dynamic_leases_table(http_client, config.BASE_URL, config.IFACE_NAME, config.EXCLUDE_HOSTNAME)
+    dhcp_leases = dhcp_dynamic_leases_table(http_client, config.BASE_URL, config.IFACE_ID, config.EXCLUDE_HOSTNAME)
+
+    if not dhcp_leases:
+        logging.info("Nothing to do ")
+        return
+
+    # Parse NTP and DNS
+    form_response = http_client.get(
+        url=urljoin(config.BASE_URL, "/services_dhcp.php"),
+        params={"if": config.IFACE_ID}
+    )
+    ntp = parse_ntp(form_response.text)
+    dns = parse_dns(form_response.text)
+
+    if not ntp:
+        logging.error("[StaticMapper] Failed to parse NTP")
+        return
+
+    if ntp and not ntp[0] and not ntp[1]:
+        logging.error("[StaticMapper] NTP is not set")
+        return
+
+    if not dns:
+        logging.error("[StaticMapper] Failed to parse DNS")
+        return
+
+    if dns and not dns[0] and not dns[1]:
+        logging.error("[StaticMapper] DNS is not set")
+        return
 
     for item in dhcp_leases:
         ipaddr = item[0]
@@ -54,7 +82,11 @@ def make_static_dhcp(http_client: httpx.Client, config: Config):
                 "hostname": hostname,
                 "descr": description,
                 "if": iface,
-            },
+                "ntp1": ntp[1],
+                "ntp2": ntp[0],
+                "dns1": dns[1],
+                "dns2": dns[0],
+            }
         )
         if make_response.is_error:
             logging.error("[StaticMapper] Failed to make static DHCP record")
