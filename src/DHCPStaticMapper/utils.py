cbebe5825aa330e8646496dd384bad6a8bd6d581
JKearnsl <boyarshinovn@gmail.com>
2024-01-17 00:18:56 +0300

    Реализованы алгоритмы парсинга dns и ntp

--- src/DHCPStaticMapper/utils.py
+++ src/DHCPStaticMapper/utils.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 
+from DHCPStaticMapper.type import IPADDR
+
 
 def parse_csrf(html: str) -> tuple[str, str] | None:
     soup = BeautifulSoup(html, "html.parser")
@@ -14,3 +16,25 @@ def parse_csrf(html: str) -> tuple[str, str] | None:
         return None
 
     return csrf_key, csrf_value
+
+
+def parse_ntp(html: str) -> tuple[IPADDR | None, IPADDR | None] | None:
+    soup = BeautifulSoup(html, "html.parser")
+    ntp_1 = soup.find("input", {"name": "ntp1"})
+    ntp_2 = soup.find("input", {"name": "ntp2"})
+
+    if not ntp_1 or not ntp_1:
+        return None
+
+    return ntp_1.attrs.get("value"), ntp_2.attrs.get("value")
+
+
+def parse_dns(html: str) -> tuple[IPADDR | None, IPADDR | None] | None:
+    soup = BeautifulSoup(html, "html.parser")
+    dns_1 = soup.find("input", {"name": "dns1"})
+    dns_2 = soup.find("input", {"name": "dns2"})
+
+    if not dns_1 or not dns_1:
+        return None
+
+    return dns_1.attrs.get("value"), dns_2.attrs.get("value")
